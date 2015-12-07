136840b86e0ee750549dc9e086f6a8088fd73921
Gokhan Dogramaci <dogramacigokhan@gmail.com>
2015-12-07 20:34:48 +0200

    Project is now compatible with python installation

---
+++ easyi3status/__init__.py
@@ -0,0 +1,82 @@
+import json
+import fileinput
+import threading
+import sys
+import time
+
+import ConfigParser, os, sys
+
+from sh import zenity, ErrorReturnCode
+
+class EasyI3Status:
+
+	sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+	modules = {}
+
+	def readModules(self):
+		elements = []
+		for modName, mod in self.modules.iteritems():
+			fetched = mod.query()
+			for it in fetched:
+				it['instance'] = modName
+				elements.append(it)
+		
+		print json.dumps(elements) + ","
+		threading.Timer(10, self.readModules).start()
+
+	def run(self):
+		print '{"version":1, "click_events": true}'
+		print '['
+
+
+		config = ConfigParser.ConfigParser()
+		config.read([
+			os.path.expanduser('~/.easyi3status/config.py')
+		])
+
+		sys.path.append(os.path.expanduser('~/.easyi3status/modules/'))
+
+		for name in config.sections():
+			module = __import__(name)
+
+			module.setup( dict(config.items(name)) )
+			self.modules[name] = module
+
+		elements = []
+
+		self.readModules()
+
+		sys.stdin.readline()
+
+		while 1:
+			line = sys.stdin.readline()
+
+			if line.startswith(","):
+				line = line[1:]
+			
+			jsonobj = json.loads(line)
+
+			module = self.modules[jsonobj['instance']]
+
+			possibleActions = module.listActions()
+			msg = module.get(jsonobj['name'])
+
+			selected = False
+			while not selected:
+				try:
+					selection = zenity("--entry", "--title", "jira", "--text", msg + "\n(" + "/".join(possibleActions) + ") ?")
+					selection = selection.strip()
+
+					if selection in possibleActions:
+						selected = True
+						method = getattr(module, selection)
+						method(jsonobj['name'])
+				except ErrorReturnCode:
+					selected = True
+
+def run():
+	app = EasyI3Status()
+	app.run()
+
+if __name__ == '__main__':
+	run()
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import find_packages, setup
+from os.path import expanduser
+from glob import glob
+
+setup(
+    # Application name:
+    name="EasyI3Status",
+
+    # Version number (initial):
+    version="0.0.1",
+
+    # Application author details:
+    author="Ahmet Emre Cepoglu",
+    author_email="aecepoglu@fastmail.fm",
+
+    # Packages
+    packages=["easyi3status"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="https://bitbucket.org/aecepoglu/easyi3status",
+
+    #
+    # license="LICENSE.txt",
+    description="Easy status bar for i3 window manager.",
+
+    long_description=open("README.md").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "ConfigParser",
+		"sh"
+    ],
+	entry_points={
+		'console_scripts': [
+			'easyi3status = easyi3status:run',
+		]
+	},
+	data_files=[
+		(expanduser("~/.easyi3status/modules"), glob("easyi3status/modules/*")),
+		(expanduser("~/.easyi3status"), ['easyi3status/config.py'])
+	],
+)
